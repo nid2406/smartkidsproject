ac0ec4173f09f5420d614b699c8cd888ab03fc2a
nid2406 <nid.2406@gmail.com>
2023-06-02 16:36:35 +0530

    skp

--- login.js
+++ login.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function validate()
 {
 	var u1 = document.getElementById("username").value;
@@ -16,4 +17,24 @@ function validate()
 			document.getElementById("msg").style.display = "block";
 		}
 
+=======
+function validate()
+{
+	var u1 = document.getElementById("username").value;
+	var p1 = document.getElementById("password").value;
+	document.getElementById("msg").style.display = "none";
+	if ( (u1 == "nidhi" && p1 == "nidhi123") || (u1 =="abcd" && p1="1234"))
+//if (u1 == "abcd" && p1=="1234")
+	{
+		document.getElementById("msg").style.display = "none";
+		alert ("Login successfully");
+
+	}
+
+		else
+		{
+			document.getElementById("msg").style.display = "block";
+		}
+
+>>>>>>> Stashed changes
 }
