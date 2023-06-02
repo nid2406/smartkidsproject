4049da99c6d9a4ac03424c4c8d0f0a55a626a779
nid2406 <133502400+nid2406@users.noreply.github.com>
2023-06-02 16:27:26 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+function validate()
+{
+	var u1 = document.getElementById("username").value;
+	var p1 = document.getElementById("password").value;
+	document.getElementById("msg").style.display = "none";
+	if ( (u1 == "nidhi" && p1 == "nidhi123") || (u1 =="abcd" && p1="1234"))
+//if (u1 == "abcd" && p1=="1234")
+	{
+		document.getElementById("msg").style.display = "none";
+		alert ("Login successfully");
+
+	}
+
+		else
+		{
+			document.getElementById("msg").style.display = "block";
+		}
+
+}
